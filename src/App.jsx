1514b02112930c25ecec9205b9c4573cc766f7a3
12Isabella <81431189+12Isabella@users.noreply.github.com>
2021-09-13 14:08:58 +0200

    build basic page structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import "./App.css";
 
 function App() {
+  function checkLocation(event) {
+    event.preventDefault();
+  }
   return (
     <div className="App">
-      <h1>Velkommen til Oslo!</h1>
+      <h1 className="mt-3">Velkommen til Oslo!</h1>
+      <h2>Her er dine clues for å finne stedet du skal til...</h2>
+      <p>Du skal til Operahuset.</p>
+      <h3>Du vet hvor du skal? Gå, gå, gå!</h3>
+      <h2>Du er på riktig sted? Trykk på knappen! Let's see...</h2>
+      <a href="/" className="btn btn-warning mt-3" onClick={checkLocation}>
+        I got it!
+      </a>
     </div>
   );
 }
