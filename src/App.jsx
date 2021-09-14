497c3a4bd130ccf73ff8833378381cc6e362f708
12Isabella <81431189+12Isabella@users.noreply.github.com>
2021-09-14 12:49:17 +0200

    add image

--- src/App.jsx
+++ src/App.jsx
@@ -23,9 +23,10 @@ function App() {
       <h1 className="mt-3">Velkommen til Oslo!</h1>
       <h2>Her er dine clues for å finne stedet du skal til...</h2>
       <p>Du skal til Operahuset.</p>
-      <h3>Du vet hvor du skal? Gå, gå, gå!</h3>
+      <img src=" /OsloOpera.jpg" alt="OsloOpera" width="300px" />
+      <h3 className="mt-2">Du vet hvor du skal? Gå, gå, gå!</h3>
       <h2>Du er på riktig sted? Trykk på knappen! Let's see...</h2>
-      <a href="/" className="btn btn-warning mt-3" onClick={checkLocation}>
+      <a href="/" className="btn btn-warning mt-3 mb-3" onClick={checkLocation}>
         I got it!
       </a>
     </div>
