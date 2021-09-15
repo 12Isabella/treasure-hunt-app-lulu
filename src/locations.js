f7831f1e6b6cb3c4451c964e19a8ce475785a77f
12Isabella <81431189+12Isabella@users.noreply.github.com>
2021-09-15 12:46:10 +0200

    add Frognerpark as location, add img for Peloton

--- src/locations.js
+++ src/locations.js
@@ -1,7 +1,7 @@
 const locations = [
   {
     name: "OsloOpera",
-    info: "Du skal til Operahuset.",
+    info: "Du skal se på havet. Men står på hvit stein mens du gjør det.",
     image: "/OsloOpera.jpg",
     coordinates: {
       northWest: { lat: 59.9094, lon: 10.7493 },
@@ -15,6 +15,15 @@ const locations = [
     coordinates: {
       northWest: { lat: 59.9176, lon: 10.7539 },
       southEast: { lat: 59.9171, lon: 10.7552 },
+    },
+  },
+  {
+    name: "Frognerpark",
+    info: "Du skal se nakene mennesker. Men dem er ikke levende...",
+    image: "/Frogner.jpg",
+    coordinates: {
+      northWest: { lat: 59.9351, lon: 10.6812 },
+      southEast: { lat: 59.9218, lon: 10.7137 },
     },
   },
 ];
