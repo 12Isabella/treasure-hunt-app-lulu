1dea247b411456bb05ae569e262b21e85b11d855
12Isabella <81431189+12Isabella@users.noreply.github.com>
2021-09-17 18:05:29 +0200

    add more places

--- src/locations.js
+++ src/locations.js
@@ -1,32 +1,62 @@
 const locations = [
   {
     index: 0,
-    name: "OsloOpera",
-    info: "Du skal se på havet. Men står på hvit stein mens du gjør det.",
-    image: "/OsloOpera.jpg",
+    name: "OsloSlott",
+    info: "Du skal til et sted som har hatt minst tre adresser. En person som bor her har en oljeplattform oppkalt etter seg.",
+    image: "/Crocus.jpg",
     coordinates: {
-      northWest: { lat: 59.9094, lon: 10.7493 },
-      southEast: { lat: 59.9051, lon: 10.7568 },
+      northWest: { lat: 59.9183, lon: 10.7255 },
+      southEast: { lat: 59.916, lon: 10.7301 },
     },
   },
   {
     index: 1,
-    name: "Peloton",
-    info: "Du skal spise Pizza.",
-    image: "/Peloton.jpg",
+    name: "FragranceOf the Heart",
+    info: "Du skal få lov å ta deg en Kaffee først! Her hadde en bestemmt Indisk Guru også gjerne slått seg ned. Han du ser på bildet har blant annet opphøyet en som kjempet i ringen og en som kjempet for frihet! Pass på å ikke havne for nært ved havet!",
+    image: "/sri-chinmoy.jpg",
     coordinates: {
-      northWest: { lat: 59.9176, lon: 10.7539 },
-      southEast: { lat: 59.9171, lon: 10.7552 },
+      northWest: { lat: 59.9275, lon: 10.7155 },
+      southEast: { lat: 59.9373, lon: 10.7165 },
     },
   },
   {
     index: 2,
     name: "Frognerpark",
     info: "Du skal se nakene mennesker. Men dem er ikke levende...",
-    image: "/Frogner.jpg",
+    image: "/Baby.jpg",
     coordinates: {
       northWest: { lat: 59.9351, lon: 10.6812 },
       southEast: { lat: 59.9218, lon: 10.7137 },
+    },
+  },
+  {
+    index: 3,
+    name: "Damplassen",
+    info: "En svensk fiskedamm er ikke på monopolbrettet, men stedet hvor den ligger har sitt eget felt.",
+    image: "/Pizza.jpg",
+    coordinates: {
+      northWest: { lat: 59.9438, lon: 10.7329 },
+      southEast: { lat: 59.9432, lon: 10.7343 },
+    },
+  },
+  {
+    index: 4,
+    name: "EvaldRyghsPlass",
+    info: "Du skal til et sted, hvor du kan både leke og minnes en trøndersk ordfører som var glad i penger og kultur. Men hvis du vil hoppe så er du på villspor.",
+    image: "/EvaldRygh.jpg",
+    coordinates: {
+      northWest: { lat: 59.9295, lon: 10.7469 },
+      southEast: { lat: 59.929, lon: 10.7478 },
+    },
+  },
+  {
+    index: 5,
+    name: "Peloton",
+    info: "Du skal spise Pizza.",
+    image: "/Peloton.jpg",
+    coordinates: {
+      northWest: { lat: 59.9176, lon: 10.7539 },
+      southEast: { lat: 59.9171, lon: 10.7552 },
     },
   },
 ];
