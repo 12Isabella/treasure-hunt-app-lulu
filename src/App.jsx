09d3c25df776bdea8a681d90e90af37fbf17b121
12Isabella <81431189+12Isabella@users.noreply.github.com>
2021-09-28 10:44:08 +0200

    clean if/else statement, set different backgrounds for each clue

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 
 function App() {
   let [locationIndex, setLocationIndex] = useState(0);
+  let [backgroundGradient, setBackgroundGradient] = useState("gradient0");
 
   const currentLocation = locations[locationIndex];
 
@@ -37,12 +38,11 @@ function App() {
         if (foundLocation.index === locations.length - 1) {
           setLocationIndex(0);
           console.log("User is at end loactaion.");
+          setBackgroundGradient(`gradient${locationIndex}`);
         } else if (locationIndex < locations.length - 1) {
           setLocationIndex(foundLocation.index + 1);
           console.log("User is at " + foundLocation.name);
-        } else {
-          setLocationIndex(0);
-          console.log("User is at first location.");
+          setBackgroundGradient(`gradient${locationIndex + 1}`);
         }
       } else {
         alert("Du er på feil sted.");
@@ -59,19 +59,26 @@ function App() {
   }
   return (
     <div className="App">
-      <h1 className="mt-3">Velkommen til Oslo!</h1>
-      <h2>Her er dine clues for å finne stedet du skal til...</h2>
-      <p>{currentLocation.info}</p>
-      <img
-        src={currentLocation.image}
-        alt={currentLocation.name}
-        width="300px"
-      />
-      <h3 className="mt-2">Du vet hvor du skal? Gå, gå, gå!</h3>
-      <h2>Du er på riktig sted? Trykk på knappen! Let's see...</h2>
-      <a href="/" className="btn btn-warning mt-3 mb-3" onClick={checkLocation}>
-        I got it!
-      </a>
+      <div className={`container ${backgroundGradient}`}>
+        <h1 className="mt-3">Velkommen til Oslo!</h1>
+        <h2>Her er dine clues for å finne stedet du skal til...</h2>
+        <p>{currentLocation.info}</p>
+        <img
+          src={currentLocation.image}
+          alt={currentLocation.name}
+          width="300px"
+          className="image"
+        />
+        <h3 className="mt-2">Du vet hvor du skal? Gå, gå, gå!</h3>
+        <h2>Du er på riktig sted? Trykk på knappen! Let's see...</h2>
+        <a
+          href="/"
+          className="btn btn-warning mt-3 mb-3"
+          onClick={checkLocation}
+        >
+          I got it!
+        </a>
+      </div>
     </div>
   );
 }
